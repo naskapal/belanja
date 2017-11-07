87cbf26b7a3fdcd6e3e0bae509edcecf92bf9e04
Rizky <co.terbang@gmail.com>
2017-11-07 15:24:38 +0700

    added partials

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.set('views', './views')
+app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index');
 })
 app.listen(3000, function () {
   console.log('IT WORKS!');
