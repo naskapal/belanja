5c382f636401704128f22b9251c50032012b48aa
alangmahendra <alangmahemdra@gmail.com>
2017-11-08 10:22:02 +0700

    first commit

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
+app.use(express.static("views"))
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('editprofile');
 })
 app.listen(3000, function () {
   console.log('IT WORKS!');
